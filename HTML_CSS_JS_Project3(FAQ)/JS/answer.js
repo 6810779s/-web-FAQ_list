2a331b313b62bf60903647b71733d366c387a41d
Eunhee <6810779s@naver.com>
2021-06-03 17:57:51 +0900

    fix some bugs like when I clicked two of different questions, there had overlapping p elements.

--- HTML_CSS_JS_Project3(FAQ)/JS/answer.js
+++ HTML_CSS_JS_Project3(FAQ)/JS/answer.js
@@ -13,33 +13,26 @@ let arr =[0,0,0,0];
 
 items[0].addEventListener('click',()=>{
   if(arr[0]==0){
-    arrows_bottom[0].style.display = "none";
-    arrows_top[0].style.display = "block";
+    remove(0);
     textNode = document.createTextNode("This project for practice css, html and Javascript.");
     ans.appendChild(textNode);
     items[0].appendChild(ans_area);
-    arr[0]=1;
-    console.log(arr[0]);
   }else{
     arrows_top[0].style.display = "none";
     arrows_bottom[0].style.display = "block"
     ans.removeChild(textNode);
     items[0].removeChild(ans_area);
     arr[0]=0;
-    console.log(arr[0]);
   }
   
 }); 
 
 items[1].addEventListener('click',()=>{
   if(arr[1]==0){
-    arrows_bottom[1].style.display = "none";
-    arrows_top[1].style.display = "block";
+    remove(1);
     textNode = document.createTextNode("I learned how to use dom in Javascript, but I think I'm still not good at Javascript. So I have to study more.");
     ans.appendChild(textNode);
-    
     items[1].appendChild(ans_area);
-    arr[1]=1;
   }else{
     arrows_top[1].style.display = "none";
     arrows_bottom[1].style.display = "block"
@@ -54,13 +47,10 @@ ans_area.appendChild(ans);
 
 items[2].addEventListener('click',()=>{
   if(arr[2]==0){
-    arrows_bottom[2].style.display = "none";
-    arrows_top[2].style.display = "block";
+    remove(2);
     textNode = document.createTextNode("2021/06/03, and made by Eunhee Jang.");
     ans.appendChild(textNode);
-    
     items[2].appendChild(ans_area);
-    arr[2]=1;
   }else{
     arrows_top[2].style.display = "none";
     arrows_bottom[2].style.display = "block"
@@ -73,12 +63,10 @@ items[2].addEventListener('click',()=>{
 
 items[3].addEventListener('click',()=>{
   if(arr[3]==0){
-    arrows_bottom[3].style.display = "none";
-    arrows_top[3].style.display = "block";
+    remove(3);
     textNode = document.createTextNode("You can press the question which you want to see the answer. And if you want to add more questions and answers, you just edit the the Javascript. In the js file, you can create more elements to make the Q&A.");
     ans.appendChild(textNode);
     items[3].appendChild(ans_area);
-    arr[3]=1;
   }else{
     arrows_top[3].style.display = "none";
     arrows_bottom[3].style.display = "block"
@@ -88,4 +76,37 @@ items[3].addEventListener('click',()=>{
   }
   
 }); 
+
+function remove(x){
+  arr[x]=1;
+  let num=x;
+  let num_cnt=0;
+
+  for (let i=0; i<arr.length;i++){
+    if (arr[i] !== 0){
+
+      if(x !== i){
+        num=i;
+        num_cnt+=1;
+      }
+    }
+  }
+  if(num_cnt>0){
+    ans.removeChild(textNode);
+    for (let i=0; i<arr.length;i++){
+      
+      if(i ===x ){
+        continue
+      }
+      else{
+        arr[i] = 0;
+      } 
+    }
+    items[num].removeChild(ans_area);
+    arrows_bottom[num].style.display = "block";
+    arrows_top[num].style.display = "none";
+}   
+    arrows_bottom[x].style.display = "none";
+    arrows_top[x].style.display = "block";
+}
 
